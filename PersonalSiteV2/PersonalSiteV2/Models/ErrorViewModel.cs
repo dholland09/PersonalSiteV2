bb4bedc16d5582093d5215cb82a60818f9b4b64b
Derek Holland <dholland@live.com>
2022-05-31 12:21:41 -0500

    added pdf download for resume

---
+++ PersonalSiteV2/PersonalSiteV2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalSiteV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
